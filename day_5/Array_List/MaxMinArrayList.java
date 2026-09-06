78d3e1d663a6af2201f447c9fa04a6f0ee3a8c78
Akhil Reddy <akhilreddy.d5@gmail.com>
2026-09-06 09:35:07 +0530

    finding min,max in arraylist

--- day_5/Array_List/MaxMinArrayList.java
+++ day_5/Array_List/MaxMinArrayList.java
@@ -8,6 +8,27 @@ import java.util.Arrays;
 public class MaxMinArrayList {
     public static void main(String[] args) {
 
-      
+        ArrayList<Integer> list =
+                new ArrayList<>(Arrays.asList(40, 10, 80, 20, 60));
+
+        int max = list.get(0);
+        int min = list.get(0);
+
+        for (int num : list) {
+
+            if (num > max)
+                max = num;
+
+            if (num < min)
+                min = num;
+        }
+
+        System.out.println("Maximum = " + max);
+        System.out.println("Minimum = " + min);
+        System.out.println("using collections");
+    System.out.println(Collections.max(list));
+    System.out.println(Collections.min(list));
     }
 }
+
+

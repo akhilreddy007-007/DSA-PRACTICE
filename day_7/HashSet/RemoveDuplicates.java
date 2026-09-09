dcd1bf00498a7534938382e623abe90737f8eac9
Akhil Reddy <akhilreddy.d5@gmail.com>
2026-09-09 07:04:21 +0530

    Remove Duplicate elements for array using HashSet

--- day_7/HashSet/RemoveDuplicates.java
+++ day_7/HashSet/RemoveDuplicates.java
@@ -3,13 +3,14 @@ import java.util.Arrays;
 import java.util.HashSet;
 public class RemoveDuplicates {
 public static void main(String[] args) {
+	//change your source array type from the primitive int[] to the wrapper Integer[] 
 	Integer []a = {1,2,3,4,5,5};
 	HashSet <Integer>hs=new HashSet<>(Arrays.asList(a));
 	System.out.println(hs);
 	
 	// or
 	
-
+//Use Java Streams to box the primitive array elements into wrapper objects before passing them to the set
 int []b= {1,2,3,4,5,5};
 HashSet <Integer>hs1=new HashSet<>(Arrays.stream(b).boxed().toList());
 	System.out.println(hs1);

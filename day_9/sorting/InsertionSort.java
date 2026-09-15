5daeba19a0ac9a8435b1e7d462dc73e26043578e
Akhil Reddy <akhilreddy.d5@gmail.com>
2026-09-15 16:03:13 +0530

    Insertion Sort

---
+++ day_9/sorting/InsertionSort.java
@@ -0,0 +1,32 @@
+package day_9.sorting;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,4,3,2,1};
+
+        insertionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
